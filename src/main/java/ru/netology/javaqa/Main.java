d232572efd20016bfd91861d8d84cb47eab070ea
Kostya Lachin <prikolist04@mail.ru>
2025-03-14 20:33:17 +0300

    initial commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,24 @@
+package ru.netology.javaqa;
+
+    public class Main {
+        public static void main(String[] args) {
+
+            String[] names = {
+                    "Petya",
+                    "Anya",
+                    "Olya",
+                    "Kolya"
+            };
+            String newName = "Slava";
+
+            String[] tmp = new String[names.length + 1];
+            for (int i = 0; i < names.length; i++) {
+                tmp[i] = names[i];
+            }
+            tmp[tmp.length - 1] = newName;
+            names = tmp;
+
+            //names = [..., "Slava"]
+        }
+    }
+}
